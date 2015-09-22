2e2660fd7a19595025cb0164df0a81fda77430cc
ellenaua <elenasharovar@gmail.com>
2015-09-22 16:31:05 +0300

    Component refactoring & code style fixed

--- spec/helpers/metadata.spec.js
+++ spec/helpers/metadata.spec.js
@@ -1,10 +1,10 @@
 describe('Metadata to select fields conversion', function () {
 
-    var metadataToSelectFields = require('../../lib/helpers/metadataToSelectFields');
+    var metadata = require('../../lib/helpers/metadata');
 
     it('should convert metadata out properties to a comma separated string value', function () {
 
-        var metadata = {
+        var input = {
             description: 'Contact',
             type: 'object',
             properties: { LastName: {},
@@ -41,14 +41,14 @@ describe('Metadata to select fields conversion', function () {
             }
         };
 
-        var result = metadataToSelectFields(metadata);
+        var result = metadata.pickSelectFields(input);
         expect(result).toEqual("LastName,FirstName,Salutation,OtherStreet,OtherCity,OtherState,OtherPostalCode,OtherCountry,MailingStreet,MailingCity,MailingState,MailingPostalCode,MailingCountry,Phone,Fax,MobilePhone,HomePhone,OtherPhone,AssistantPhone,Email,Title,Department,AssistantName,LeadSource,Birthdate,Description,EmailBouncedReason,EmailBouncedDate,Jigsaw,Level__c,Languages__c");
     });
 
     it('should throw an exception if metadata.out doesn\'t exist', function () {
 
         expect(function () {
-            metadataToSelectFields({});
+            metadata.pickSelectFields({});
         }).toThrow(new Error('No out metadata found to create select fields from'));
 
     });
