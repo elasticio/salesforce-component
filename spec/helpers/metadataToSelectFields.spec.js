48b9f674c8266c7764feee5f7ee1ba8715260a99
ellenaua <elenasharovar@gmail.com>
2015-09-22 16:22:19 +0300

    Component refactoring & code style fixed

--- spec/helpers/metadataToSelectFields.spec.js
+++ spec/helpers/metadataToSelectFields.spec.js
@@ -1,6 +1,7 @@
-var metadataToSelectFields = require('../../lib/helpers/metadataToSelectFields');
-
 describe('Metadata to select fields conversion', function () {
+
+    var metadataToSelectFields = require('../../lib/helpers/metadataToSelectFields');
+
     it('should convert metadata out properties to a comma separated string value', function () {
 
         var metadata = {
@@ -40,11 +41,8 @@ describe('Metadata to select fields conversion', function () {
             }
         };
 
-        var params = {};
-        params.metadata = metadata;
-        var result = metadataToSelectFields(params);
-
-        expect(result.selectFields).toEqual("LastName,FirstName,Salutation,OtherStreet,OtherCity,OtherState,OtherPostalCode,OtherCountry,MailingStreet,MailingCity,MailingState,MailingPostalCode,MailingCountry,Phone,Fax,MobilePhone,HomePhone,OtherPhone,AssistantPhone,Email,Title,Department,AssistantName,LeadSource,Birthdate,Description,EmailBouncedReason,EmailBouncedDate,Jigsaw,Level__c,Languages__c");
+        var result = metadataToSelectFields(metadata);
+        expect(result).toEqual("LastName,FirstName,Salutation,OtherStreet,OtherCity,OtherState,OtherPostalCode,OtherCountry,MailingStreet,MailingCity,MailingState,MailingPostalCode,MailingCountry,Phone,Fax,MobilePhone,HomePhone,OtherPhone,AssistantPhone,Email,Title,Department,AssistantName,LeadSource,Birthdate,Description,EmailBouncedReason,EmailBouncedDate,Jigsaw,Level__c,Languages__c");
     });
 
     it('should throw an exception if metadata.out doesn\'t exist', function () {
