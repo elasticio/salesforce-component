c2789799fee21f4ffc3924a3d2f44335a01944d0
ShkarupaNick <shkarupanick@gmail.com>
2018-11-08 15:40:57 +0200

    added debug token

--- verifyCredentials.js
+++ verifyCredentials.js
@@ -1,42 +1,45 @@
-var request = require('request');
-var NOT_ENABLED_ERROR = 'Salesforce respond with this error: "The REST API is not enabled for this Organization."';
-var VERSION = "v32.0";
-var debug = require('debug')('verifyCredentials');
+const request = require('request');
+const NOT_ENABLED_ERROR = 'Salesforce respond with this error: "The REST API is not enabled for this Organization."';
+const VERSION = 'v32.0';
+const debug = require('debug')('verifyCredentials');
+const debugToken = require('debug')('token');
 
 module.exports = verify;
 
 function verify(credentials, cb) {
-    if (!credentials.oauth || credentials.oauth.error) {
-        return cb(null, {verified: false});
+  debugToken(credentials);
+  if (!credentials.oauth || credentials.oauth.error) {
+    return cb(null, {verified: false});
+  }
+  const token = credentials.oauth.access_token;
+  const url = credentials.oauth.instance_url + '/services/data/' + VERSION +
+      '/sobjects';
+
+  debug('To verify credentials send request to %s', url);
+
+  const options = {
+    url: url,
+    headers: {
+      Authorization: 'Bearer ' + token,
+    },
+  };
+
+  request.get(options, checkResponse);
+
+  function checkResponse(err, response, body) {
+    if (err) {
+      return cb(err);
     }
-    var token = credentials.oauth.access_token;
-    var url = credentials.oauth.instance_url + '/services/data/' + VERSION + '/sobjects';
-
-    debug('To verify credentials send request to %s', url);
-
-    var options = {
-        url: url,
-        headers: {
-            Authorization: 'Bearer ' + token
-        }
-    };
-
-    request.get(options, checkResponse);
-
-    function checkResponse(err, response, body) {
-        if (err) {
-            return cb(err);
-        }
-        debug('Salesforce response was: %s %j', response.statusCode, body);
-        if (response.statusCode === 401) {
-            return cb(null, {verified: false});
-        }
-        if (response.statusCode === 403) {
-            return cb(null, {verified: false, details: NOT_ENABLED_ERROR});
-        }
-        if (response.statusCode !== 200) {
-            return cb(new Error('Salesforce respond with ' + response.statusCode));
-        }
-        cb(null, {verified: true});
+    debug('Salesforce response was: %s %j', response.statusCode, body);
+    if (response.statusCode === 401) {
+      return cb(null, {verified: false});
     }
+    if (response.statusCode === 403) {
+      return cb(null, {verified: false, details: NOT_ENABLED_ERROR});
+    }
+    if (response.statusCode !== 200) {
+      return cb(new Error('Salesforce respond with ' + response.statusCode));
+    }
+    cb(null, {verified: true});
+  }
 }
