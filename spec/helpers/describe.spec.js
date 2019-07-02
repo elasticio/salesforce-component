ebf9515aeaf30e88403f6ae88173e474f2821fa9
Olha Virolainen <olga.shukel@gmail.com>
2019-07-02 17:14:13 +0300

    update unit and integration test

--- spec/helpers/describe.spec.js
+++ spec/helpers/describe.spec.js
@@ -1,43 +1,52 @@
-describe('Saleforce Describe', function () {
+const nock = require('nock');
+const chai = require('chai');
 
-    var salesforceDescribe = require('../../lib/helpers/describe').describeObject;
-    var nock = require('nock');
-    var description = require('../objectDescription.json');
+const { expect } = chai;
+const { describeObject, fetchObjectTypes } = require('../../lib/helpers/describe');
+const description = require('../testData/objectDescription.json');
+const objectLists = require('../testData/objectsList.json');
 
-    it('should fetch a description for the specified object type', function () {
+const cfg = {
+  oauth: {
+    access_token: '00DE0000000dwKc!ARcAQEgJMHustyszwbrh06CGCuYPn2aI..bAV4T8aA8aDXRpAWeveWq7jhUlGl7d2e7T8itqCX1F0f_LeuMDiGzZrdOIIVnE',
+    signature: 'BVzAhWIq0NNzW+H0ehXq5ddLAPAX1J5wMmbpqHiURsQ=',
+    refresh_token: '5Aep861rEpScxnNE66jGO6gqeJ82V9qXOs5YIxlkVZgWYMSJfjLeqYUwKNfA2R7cU04EyjVKE9_A.vqQY9kjgUg',
+    token_type: 'Bearer',
+    instance_url: 'https://na9.salesforce.com',
+    scope: 'id api refresh_token',
+    issued_at: '1396940795887',
+    id: 'https://login.salesforce.com/id/00DE0000000dwKcMAI/005E0000002CozkIAC',
+  },
+  object: 'Contact',
+  apiVersion: 'v25.0',
+};
 
-        var params = {};
-        params.cfg = {
-            oauth: {
-                access_token: "00DE0000000dwKc!ARcAQEgJMHustyszwbrh06CGCuYPn2aI..bAV4T8aA8aDXRpAWeveWq7jhUlGl7d2e7T8itqCX1F0f_LeuMDiGzZrdOIIVnE",
-                signature: "BVzAhWIq0NNzW+H0ehXq5ddLAPAX1J5wMmbpqHiURsQ=",
-                refresh_token: "5Aep861rEpScxnNE66jGO6gqeJ82V9qXOs5YIxlkVZgWYMSJfjLeqYUwKNfA2R7cU04EyjVKE9_A.vqQY9kjgUg",
-                token_type: "Bearer",
-                instance_url: "https://na9.salesforce.com",
-                scope: "id api refresh_token",
-                issued_at: "1396940795887",
-                id: "https://login.salesforce.com/id/00DE0000000dwKcMAI/005E0000002CozkIAC"
-            },
-            object: 'Contact',
-            apiVersion: "v25.0"
-        };
+describe('Describe helper', () => {
+  it('describeObject should fetch a description for the specified object type', async () => {
+    nock('https://na9.salesforce.com')
+      .get('/services/data/v25.0/sobjects/Contact/describe')
+      .reply(200, JSON.stringify(description));
 
-        var result;
+    const result = await describeObject({ cfg });
+    expect(result).to.deep.equal(description);
+  });
 
-        nock('https://na9.salesforce.com').get('/services/data/v25.0/sobjects/Contact/describe').reply(200, JSON.stringify(description));
+  it('should fetch a description for the specified object type', (done) => {
+    nock('https://na9.salesforce.com')
+      .get('/services/data/v25.0/sobjects')
+      .reply(200, objectLists);
 
-        runs(function () {
-            salesforceDescribe(params).then(function (description) {
-                result = description;
-            });
+    fetchObjectTypes(cfg, (err, result) => {
+      try {
+        expect(err).to.equal(null);
+        expect(result).to.deep.equal({
+          Account: 'Account',
+          AccountContactRole: 'Account Contact Role',
         });
-
-        waitsFor(function () {
-            return result;
-        });
-
-        runs(function () {
-            expect(result).toEqual(JSON.parse(JSON.stringify(description)));
-        });
+        done();
+      } catch (e) {
+        done(e);
+      }
     });
+  });
 });
