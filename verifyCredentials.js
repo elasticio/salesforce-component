ff0332a526cb22091f6b6370cfb10c6267b8a179
denis.gladikov <denis.gladikov@elastic.io>
2019-11-06 10:54:24 +0200

    Add oauth envars check to verifyCredentials script

--- verifyCredentials.js
+++ verifyCredentials.js
@@ -1,12 +1,21 @@
 const request = require('request');
+const fs = require('fs');
 
 const NOT_ENABLED_ERROR = 'Salesforce respond with this error: "The REST API is not enabled for this Organization."';
 const VERSION = 'v32.0';
 const debug = require('debug')('verifyCredentials');
 const debugToken = require('debug')('token');
 
+if (fs.existsSync('.env')) {
+  // eslint-disable-next-line global-require
+  require('dotenv').config();
+}
+
 // eslint-disable-next-line consistent-return
 module.exports = function verify(credentials, cb) {
+// eslint-disable-next-line no-use-before-define
+  checkOauth2EnvarsPresence();
+
   function checkResponse(err, response, body) {
     if (err) {
       return cb(err);
@@ -41,3 +50,14 @@ module.exports = function verify(credentials, cb) {
 
   request.get(options, checkResponse);
 };
+
+function checkOauth2EnvarsPresence() {
+  if (!process.env.OAUTH_CLIENT_ID) {
+    if (!process.env.OAUTH_CLIENT_SECRET) {
+      throw new Error('Environment variables are missed: OAUTH_CLIENT_ID, OAUTH_CLIENT_SECRET');
+    }
+    throw new Error('Environment variables are missed: OAUTH_CLIENT_ID');
+  } else if (!process.env.OAUTH_CLIENT_SECRET) {
+    throw new Error('Environment variables are missed: OAUTH_CLIENT_SECRET');
+  }
+}
