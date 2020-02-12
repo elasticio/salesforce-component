3ca0db5f062b6feecdcd2e233b758337bb572134
christianjosephvarriale <cjvarria@uwaterloo.ca>
2020-02-12 15:13:38 +0100

    changed syntax error

--- spec-integration/actions/deleteObject.spec.js
+++ spec-integration/actions/deleteObject.spec.js
@@ -21,7 +21,8 @@ describe('Delete Object Integration Functionality', () => {
       logger,
     };
 
-    if (!( testObjLst = testDataFactory.call(emitter) )) {
+    testObjLst = await testDataFactory.call(emitter);
+    if (!( testObjLst )) {
       throw Error('Test data was not sucessfully created');
     }
   });
@@ -36,28 +37,16 @@ describe('Delete Object Integration Functionality', () => {
   });
 
   it('Correctly deletes an existent Contact', async () => {
-    testObj = await testObjGen.next() 
-    if (!( testObj.value )) {
-      throw Error('Test data was not sucessfully created');
-    }
     await deleteObject.process.call(emitter, testObjLst[1].message, testObjLst[1].config)
     expect(emitter.emit.args[2][1].body).to.be.deep.equal(testObj.response);
   });
 
   it('Correctly identifies when more than one object is found and throws', async () => {
-    testObj = await testObjGen.next() 
-    if (!( testObj.value )) {
-      throw Error('Test data was not sucessfully created');
-    }
     await deleteObject.process.call(emitter, testObjLst[2].message, testObjLst[2].config)
     expect(emitter.emit.args[2][1]).to.be.instanceOf(Error);
   });
 
   it('Correctly deletes based on different criteria: allFields ( Date/Time )', async () => {
-    testObj = await testObjGen.next() 
-    if (!( testObj.value )) {
-      throw Error('Test data was not sucessfully created');
-    }
     await deleteObject.process.call(emitter, testObjLst[3].message, testObjLst[3].config)
     expect(emitter.emit.args[2][1].body).to.be.deep.equal(testObj.response);
   });
